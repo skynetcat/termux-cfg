ee55a41cfd067c040e004446f614e398a1c33f8b
skynetcat <188181928+skynetcat@users.noreply.github.com>
2025-05-17 23:32:07 +0300

    Improved nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,12 @@
+
 -- Colorscheme
 vim.cmd.colorscheme 'desert'
 
 -- Set space as the leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+require("config.lazy")
 
 -- Enable line numbers and relative line numbers
 vim.wo.number = true
@@ -84,7 +87,7 @@ vim.keymap.set({'i', 'c'}, '/', function()
 end, {silent = true, expr = true})
 --]]
 
-vim.keymap.set({'i', 'c'}, '<C-f>', '<C-x><C-f><C-p>', {silent = true})
+vim.keymap.set({'i', 'c'}, '<C-f>', '<C-x><C-f>', {silent = true})
 
 
 
@@ -135,4 +138,56 @@ vim.keymap.set('n', '<leader>h', '<Cmd>:10 split term://%:p:h//bash<CR>:wincmd j
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Close buffer
-vim.keymap.set('n', '<leader>c', '<Cmd>:bd!<CR>', {silent = true})
+vim.keymap.set('n', '<leader>fq', '<Cmd>:bd!<CR>', {silent = true})
+
+-- Treesitter
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- COQ Nvim
+vim.keymap.set("n", "<leader>cr", "<cmd>:COQsnips compile<CR>", {})
+vim.keymap.set("n", "<leader>ce", "<cmd>:COQsnips edit<CR>", {})
+
+vim.keymap.set("n", "<leader>q", "<cmd>:w<CR>:bd<CR>", {})
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "bash", "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
